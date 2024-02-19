770ea4d4ae83d188676620532b3acbcec0edb4d5
shubhat33 <rashidshubhat41@gmail.com>
2024-02-19 18:44:31 +0530

    question 3 function for loop and settimeout question and solution

--- closures/interviewQuestions.js
+++ closures/interviewQuestions.js
@@ -56,5 +56,43 @@
 
 // QUESTION 03 :
 {
-    
+    function A(){
+        console.log('start')
+        for(var i=0;i<3;i++){
+            setTimeout(()=>{
+                console.log(i)
+            },1000*i)
+        }
+        console.log('end')
+    }
+    A()
+    // What is the output of this code ?
+
+    // SOLUTION ;
+
+    // output ;
+    // start
+    // end
+    // 3
+    // 3
+    // 3
+    // Because var has functional scope and setTimeout is asynchronous and hence runs after the
+    // code has finished(even if time is 0) where it encounters value of i as 3 because thats where loop ends.
+    // we can get 0,1,2 by replacing var with let as let hash block scope.
+    // we can also do the same using closures i.e
+    // function A(){
+    //     console.log('start')
+        
+    //     for(var i=0;i<3;i++){
+    //         function inner(i) {
+    //             setTimeout(()=>{
+    //                 console.log(i)
+    //             },1000*i)
+    //         }
+    //         inner(i)
+    //     }
+
+    //     console.log('end')
+    // }
+
 }

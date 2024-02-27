f4e384215e4df83cabe557e9cfca548b7cf96656
shubhat33 <rashidshubhat41@gmail.com>
2024-02-27 20:27:55 +0530

    second question

--- objects/questions.js
+++ objects/questions.js
@@ -9,3 +9,21 @@
     // console.log(obj)
     // solution : in output a will be 4 as it is reassigned a value 4 later
 }
+
+// QUESTION : 02
+{
+    const obj = {
+        a : 100,
+        b : 200,
+        c : 'hello'
+    }
+
+    // multiply numeric elements by 
+    // solution
+    for (key in obj){
+        if (typeof obj[key] == 'number'){
+            obj[key] *= 2
+        }
+    }
+    console.log(obj)
+}

750710fd5cc0303c7bf92b3c533e9497b7ff760d
shubhat33 <rashidshubhat41@gmail.com>
2024-03-04 22:06:02 +0530

    add more questions on output

--- objects/questions.js
+++ objects/questions.js
@@ -47,3 +47,79 @@
         // is 456
 
 }
+
+// QUESTION : 04
+{
+    // console.log([...'shubhat'])
+    // guess output
+    // ans : spreads all the charactes of the string
+}
+
+// QUESTION : 05 
+{
+    const shape = {
+        radius:10,
+        diameter(){
+            return this.radius*2;
+        },
+        perimeter: () => {
+            console.log(this.radius)
+            return this.radius *2 * Math.PI
+        }
+    }
+    // what would be the output of this code
+    // console.log(shape.diameter()) -> 20
+    // console.log(shape.perimeter()) -> NaN
+    // arrow function doesnot support 'this' and 'args keyword,
+    // this keyword in arrow function refers to window object
+}
+
+// QUESTION : 06 
+{
+    // Destructuring
+    let user = {
+        Name : 'shubhat',
+        age : 24,
+        hobbies : {
+            game : 'cricket',
+            work : 'coding',
+            extra : 'cycling'
+        }
+    }
+
+    // a) Destructure the object
+            // const {Name,age} = user
+            // console.log(Name)
+
+    // b) If a variable of name same as property of object exists
+    //    How to change the destructured property name then?
+            // let Name = 'coding'
+            // const {Name:firstName,age} = user
+            // console.log(firstName,Name)
+
+    // c) Destructure hobbies objects properties
+            //  const {hobbies:{game,work,extra}} = user
+            //  console.log(work)
+
+}
+
+// QUESTION : 07
+{
+    // function getItems(fruitList,...args,favouriteFruit){
+    //     return [...favouriteFruit,...args,favouriteFruit]
+    // }
+
+    // 1) Guess the output.
+    //      console.log(getItems(["banana","orange"],'pear','orange'))
+    //      Output will be error as rest parameter must be last but spread
+    //      can be anywhere.
+
+    // 2) if Error then correct the code and guess the output.
+          // correct code :
+          function getItems(fruitList,favouriteFruit,...args){
+            return [...fruitList,...favouriteFruit,...args]
+        }
+        console.log(getItems(["banana","orange"],'pear','orange'))
+        // Output will be 'banana','orange','p', 'e', 'a', 'r','orange'
+        
+}

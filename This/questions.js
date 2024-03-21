c1c9121e0604a82dda0a2773a82e8b633fc14151
shubhat33 <rashidshubhat41@gmail.com>
2024-03-21 14:14:43 +0530

    question 02

--- This/questions.js
+++ This/questions.js
@@ -12,3 +12,20 @@
     // Solution : Output will be 'shubhat' as this inside a function which is a 
     // method in an object refers to the object itself
 }
+
+// QUESTION 02:
+{
+    // What will be the output ?
+    const makeUser = function(){
+        return {
+            name : 'john',
+            ref : this
+        }
+    }
+
+    const newUser = makeUser()
+    // console.log(newUser.ref.name)
+    // Solution : output undefined as this will refer to window and not the object 
+    //            only incase of a method inside the object will it refer to the 
+    //            Object.
+}

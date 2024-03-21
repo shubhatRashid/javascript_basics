66a6dfe0a1c47db539eeb40e24b48b3869399c10
shubhat33 <rashidshubhat41@gmail.com>
2024-03-21 14:29:14 +0530

    more questions on this keyword

--- This/questions.js
+++ This/questions.js
@@ -29,3 +29,37 @@
     //            only incase of a method inside the object will it refer to the 
     //            Object.
 }
+
+// QUESTION 03:
+{
+    // Fix the above question to get the expected output?
+    const makeUser = function(){
+        return {
+            name : 'john',
+            ref : function() {return this}
+        }
+    }
+
+    const newUser = makeUser()
+    // console.log(newUser.ref().name) returns 'john'
+}
+
+// QUESTION 04:
+{   // what will be the output ?
+    const person = {
+        firstName : 'shubhat',
+        logName : function(){
+            console.log(this.firstName)
+        }
+    }
+    //setTimeout(person.logName,1000)
+    
+    // Output will be undefined as it is passed as a callback function and not
+    // as a method in setTimeout which doesnot have access to the person object
+    // To fix this use an anonymous function as callback and use the method inside
+    // that fucnction i.e
+
+    // setTimeout(()=>{
+    //     person.logName()
+    // },1000)
+}

062cd72939c06cee8e068a1ef658e4a010c6d7c5
shubhat33 <rashidshubhat41@gmail.com>
2024-04-20 13:19:22 +0530

    more on callbacks

--- Promises/basics.js
+++ Promises/basics.js
@@ -30,37 +30,66 @@
 // CALLBACKS : functions which are passed as arguments to other functions
 
     // QUESTION :
-    {
-        console.log('start')
+        // {
+        //     console.log('start')
 
-        function slow(userName){
-            setTimeout(() => {
-                return userName
-            },1000)
-        }
-        const message = slow('shubhat')
-        console.log(message)
-        console.log('finish')
+        //     function slow(userName){
+        //         setTimeout(() => {
+        //             return userName
+        //         },1000)
+        //     }
+        //     const message = slow('shubhat')
+        //     console.log(message)
+        //     console.log('finish')
 
-        // output :
-            // start
-            // undefined
-            // finish
+        //     // output :
+        //         // start
+        //         // undefined
+        //         // finish
 
-        // how to run this code properly ?
+        //     // how to run this code properly ?
 
-    }
+        // }
     // SOLUTION : using callbacks
-    {   
-        console.log('start')
+        // {   
+        //     console.log('start')
 
-        function slow(userName,cb){
-            setTimeout(() => {
-                return cb(userName)
-            },1000)
+        //     function slow(userName,cb){
+        //         setTimeout(() => {
+        //             return cb(userName)
+        //         },1000)
+        //     }
+        //     slow('shubhat',(message)=> {console.log(message)})
+        //     console.log('finish')
+
+        // }
+
+    // QUESTION : What if there is a function which depends on 
+    //            slow functions result, how to execute it ?
+    // SOLUTION : this is also achieved using callbacks
+        {
+            console.log('start')
+
+            function slow(userName,cb){
+                setTimeout(() => {
+                    return cb(userName)
+                },1000)
+            }
+
+            function dependent(cb){
+                setTimeout(() => {
+                    return cb()
+                },1000)
+            }
+
+            slow('shubhat',(message)=> {
+                console.log(message)
+                dependent(()=> {console.log('slow has executed')})
+            })
+
+            console.log('finish')
         }
-        slow('shubhat',(message)=> {console.log(message)})
-        console.log('finish')
-
-    }
-
+    
+    // QUESTION : What if there are more dependent function ?
+    // SOLUTION : The code gets messy and messy each time we
+    //            use a callback so we need an alternative..

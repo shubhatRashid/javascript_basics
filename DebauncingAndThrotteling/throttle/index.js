8d0fbea8d8de0fb53ebff76ecfeb93d2cc5c6797
shubhat33 <rashidshubhat41@gmail.com>
2024-04-29 19:50:27 +0530

    throttle question

--- DebauncingAndThrotteling/throttle/index.js
+++ DebauncingAndThrotteling/throttle/index.js
@@ -1,12 +1,12 @@
 let clicked = 0
 let invoked = 0
-var debounced = _.debounce(()=> {
+var throttle = _.throttle(()=> {
     invoked += 1
     document.getElementById('invoked').innerHTML = `button invoked ${invoked} times`
 },800);
 document.getElementById('button').onclick = function() {
     clicked += 1
     document.getElementById('clicked').innerHTML = `button clicked ${clicked} times`
-    debounced()
+    throttle()
  }
 

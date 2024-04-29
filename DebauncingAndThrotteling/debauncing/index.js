8ad20ce3cfb86d193209e0c66aede7ab3b50673e
shubhat33 <rashidshubhat41@gmail.com>
2024-04-29 16:49:16 +0530

    debauncing using lodash

--- DebauncingAndThrotteling/debauncing/index.js
+++ DebauncingAndThrotteling/debauncing/index.js
@@ -1,14 +1,12 @@
 let clicked = 0
 let invoked = 0
-document.getElementById('button').onclick = function() {
+var debounced = _.debounce(()=> {
+    invoked += 1
+    document.getElementById('invoked').innerHTML = `button invoked ${invoked} times`
+},800);
+document.getElementById('button').addEventListener('click',function() {
     clicked += 1
     document.getElementById('clicked').innerHTML = `button clicked ${clicked} times`
- }
+    debounced()
+ })
 
- document.getElementById('button').addEventListener('mouseleave',()=>{
-    setTimeout(()=> {
-        invoked += 1
-        document.getElementById('invoked').innerHTML = `button invoked ${invoked} times`
-    },1000)
- }
-)

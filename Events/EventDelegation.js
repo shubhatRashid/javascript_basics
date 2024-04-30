6143e50b3e4a396f942e70930a3c3c7109755917
shubhat33 <rashidshubhat41@gmail.com>
2024-04-30 16:17:47 +0530

    event delegation

---
+++ Events/EventDelegation.js
@@ -0,0 +1,5 @@
+// EVENT DELEGATION
+
+// instead of applying event listeners to all children 
+// event listener is applied to parent only then we can use 
+// target to determine where event came from
---
+++ Events/eventdelegation/index.js
@@ -0,0 +1,7 @@
+let parent = document.getElementById('products')
+parent.addEventListener('click',(event) => {
+    console.log(event.target.id)
+})
+
+
+

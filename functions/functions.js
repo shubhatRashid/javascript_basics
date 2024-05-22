9c86f24b24769de9ffc3875dca9d64691790661e
shubhat33 <rashidshubhat41@gmail.com>
2024-05-22 11:43:30 +0530

    functions basics

---
+++ functions/functions.js
@@ -0,0 +1,108 @@
+// FUNCTION DECLERATION
+    // {
+    //     function returnHello() {
+    //         return 'hello'
+    //     }
+
+    //     // with parameters
+    //     function greet(greetMsg){
+    //         return greetMsg
+    //     }
+    // }
+
+// FUNCTION EXPRESSION AND ANONYMOUS FUNCTION
+    // {
+    //     const anfun = function(){
+    //         return 'hello'
+    //     }
+    // }
+
+// FIRST CLASS FUNCTIONS
+    // {
+    //     // functions which can be treated as variables :
+    //     //     decleared as variables
+    //     //     passed in other function as argument
+    //     //     returned from other functions
+    // }
+
+// IIFE
+    // {
+    //     (function helloWorld(){
+    //         console.log('hello world')
+    //     })()
+
+    //     // functions which are decleared and called simultanously
+    //     console.log(
+    //         function displaySquare(y){
+    //         return function square(x){
+    //             return x*x
+    //         }(6)
+    //     }(7)
+
+    //     // guess return value ?
+    //     )
+    // }
+
+// ARROW FUNCTIONS
+    // {
+    //     const hello = () => {
+    //         return 'hello'
+    //     }
+    //     // simpler syntax
+    //     // generally used in callbacks
+    //     // has global or window scope for 'this' keyword
+    //     // not accessible before decleration
+    //     // donot have arguments keyword
+    //     // cannot be used to build a constructor
+    // }
+
+// HOISTING IN FUNCTIONS
+    // {   
+    //     greetme('hello world')
+    //     function greetme(msg){
+    //         console.log(msg)
+    //     }
+
+    //     // normal function are hoisted 
+    //     // ARROW functions are not hoisted as they are function expressions and like all 
+    //     // variables their declerations are hoisted and not initializations
+
+    // }
+
+// ARGUMENTS AND PARAMS
+    // {
+    //     // Values decleared at the time of decleration are parameters
+    //     // Values passed at the time of calling are arguments
+    // }
+
+// REST AND SPREAD OPERATORS
+    // {   
+    //     // REST OPERATOR TAKES ALL VALUES AND GIVES AN ARRAY OF THEM :
+    //     let myarr = [1,2,3,4]
+    //     function restfunction (...array){ // ... array is rest operator
+    //         console.log(array)
+    //     }
+    //     restfunction(1,2,3,4,5) // indivisual values given
+    //     // output : [1,2,3,4,5]
+
+
+    //     // SPREAD OPERATOR SPREADS ALL VALUES OF AN ARRAY INTO INDIVISUAL ONES
+    //     let myarr1 = [1,2,3,4]
+    //     function spreadfunction (array){ // ... array is spread operator
+    //         console.log(...array)
+    //     }
+    //     spreadfunction(myarr1) // indivisual values given
+    //     // output : 1 2 3 4 
+
+
+    // }
+
+// CALLBACK FUNCTION 
+    {
+        // FUNCTIONS WHICH ARE PASSED AS ARGUMENTS INTO OTHER FUNCTIONS
+        // EVENT LISTENERS
+        // SETTIMEOUT
+        // ARRAY METHODS
+        
+    }
+
--- functions/setTimeout.js
+++ functions/setTimeout.js
@@ -4,6 +4,8 @@ for (let i =0;i<5;i++){
     },i*1000)
 }
 
+// let has block scope which forces settimeout to use new value of i each time 
+
 // output = 0,1,2,3,4
 
 for (var i =0;i<5;i++){
@@ -19,5 +21,6 @@ for (var i =0;i<5;i++){
     })
 }
 // output = 5,5,5,5,5
+// var has functional scope so settimeout runs independent of var and runs asynchronously and hence loop end at value 5 which is printed
 // Even in 0 time settimeout runs independent of 
 // for loop as it behaves asynchronous
